84235ee60d56d47e2db18c8781ded3fc98d4f3b4
sofiaabrahamsen <sofia.abrahamsen@yahoo.com>
2025-01-31 17:56:27 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, app, render_template
+from flask_session import Session
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+###################################
+###################################
+def _________GET_________(): pass
+###################################
+###################################
+@app.get("/")
+def view_index():
+    return render_template("index.html")
+
+@app.get("/contact")
+def view_contact():
+    return render_template("contact.html")
+
+@app.get("/about")
+def view_about():
+    return render_template("about.html")
+
+##########################
+if __name__ == "__main__":
+    app.run(debug=True)
